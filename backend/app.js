4c911d55cec5cbc3090ec66e6abcb1e0e0fa5f92
Wesley Kelly <wesley.kelly@hotmail.com>
2022-04-07 12:36:04 +0100

    get cors

--- backend/app.js
+++ backend/app.js
@@ -42,11 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
-});
+
 
 
 
