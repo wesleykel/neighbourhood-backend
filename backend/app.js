184a78dae7813c108debac9814f1e8373683d26b
Wesley Kelly <wesley.kelly@hotmail.com>
2022-04-07 12:07:33 +0100

    get cors

--- backend/app.js
+++ backend/app.js
@@ -43,9 +43,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-app.use(cors({
-  origin: "*",
-}))
+app.use(cors())
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
