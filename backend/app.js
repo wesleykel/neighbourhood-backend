9e990aeb3f5ed9cb4792aeae43baa73701a1162e
Wesley Kelly <wesley.kelly@hotmail.com>
2022-04-07 12:39:15 +0100

    get cors

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ const __dirname = path.dirname(__filename);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -46,7 +46,7 @@ app.use('/users', usersRouter);
 
 
 
-app.use(cors())
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
